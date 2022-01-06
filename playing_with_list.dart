5dca84ed32f3368eda6ef0e5419ecdc538caf6a0
Tushar Asodariya <tusharasodariya1@gmail.com>
2022-01-06 21:49:24 +0530

    list - reverse,find2ndLargest,chkSorted added

--- playing_with_list.dart
+++ playing_with_list.dart
@@ -1,11 +1,11 @@
 void main() {
   List<int> numList = [];
-   numList = insert(numList,5,1);
+   numList = insert(numList,500,1);
   numList = insert(numList,4,5);
    numList = insert(numList,3,5);
   numList = insert(numList,2,5);
-   numList = insert(numList,20,6);
-   numList = insert(numList,100,5);
+   numList = insert(numList,220,6);
+   numList = insert(numList,99,5);
   numList = insert(numList,101,4);
   print(numList);
   numList = insert(numList,1,3);
@@ -15,6 +15,60 @@ print(numList);
   print(delete(numList,4));
   print(maxNumber(numList));
   
+  print(chkSorted([1,2,3,4,5]));
+  print(find2ndLargest(numList));
+  print(reverseTheList(numList));
+  
+}
+
+
+reverseTheList(List numList){
+  List<int> tempList = [];
+  for(var i = numList.length-1;i>=0;i--){
+    tempList.add(numList[i]);
+  }
+  return tempList;
+}
+
+find2ndLargest(List numList){
+  int max=numList[0];
+  int max2 = 0;
+  for(var i=0;i<numList.length;i++){
+    if(numList[i]>max){
+       max2=max;
+      max = numList[i];
+     
+    }
+    else if(numList[i]>max2 || max==max2){
+      max2 = numList[i];
+    }
+  }
+  return max2;
+}
+
+chkSorted(List numList){
+  String str = 'unsorted';
+  for(var i =0;i<numList.length;i++){
+    if(i+2<numList.length){
+      if(numList[i]>numList[i+1]){
+        if(numList[i+1]>numList[i+2]){
+          str = 'sorted';
+        }
+        else{
+          str = 'unsorted';
+        }
+      }
+      else if(numList[i]<numList[i+1]){
+        if(numList[i+1]<numList[i+2]){
+          str = 'sorted';
+        }
+        else{
+          str = 'unsorted';
+        }
+      }
+    }
+  }
+  return str;
 }
 
 maxNumber(List numList){
